38892448ebc6067e68612682c1c32a6652b23fac
Charlie Axelrod <charlieaxelrod@gmail.com>
2019-02-12 14:59:50 -0800

    Starts implementing video scrubbing

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -12,10 +12,17 @@ function adjustSlider(media, property) {
   media[property] = this.value;
 }
 
+function scrubVideo(media, scrub = false) {
+  if (scrub) media.currentTime = this.value;
+  else media.currentTime += Number(this.dataset.skip);
+}
+
 const video = document.querySelector('.viewer');
 const playButton = document.querySelector('.toggle');
 const volume = document.querySelector('input[name="volume"]');
 const speed = document.querySelector('input[name="playbackRate"]');
+const playbackButtons = document.querySelectorAll('.player__button:not(.toggle)');
+const filled = document.querySelector('.progress__filled');
 
 playButton.addEventListener('click', function(){
   let boundToggle = toggleVideo.bind(this);
@@ -31,3 +38,16 @@ speed.addEventListener('input', function() {
   let boundAdjust = adjustSlider.bind(this);
   boundAdjust(video, 'playbackRate');
 });
+
+playbackButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    let boundScrub = scrubVideo.bind(this);
+    boundScrub(video);
+  });
+});
+
+video.addEventListener('timeupdate', function() {
+  filled.style.flexBasis = `${100 * (video.currentTime/video.duration)}%`;
+});
+
+
